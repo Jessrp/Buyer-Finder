5d226e42695de51462f0bd3676c27387e91a597c
Jessrp <111854668+Jessrp@users.noreply.github.com>
2026-02-22 21:42:46 -0500

    Delete notifications.js

--- notifications.js
+++
@@ -1,7 +0,0 @@
-function notify(msg) {
-  const n = document.createElement('div');
-  n.className = 'toast';
-  n.textContent = msg;
-  notificationsContainer.appendChild(n);
-  setTimeout(() => n.remove(), 3000);
-}
